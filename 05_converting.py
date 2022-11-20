a477d825a9e985752d7bf60946d7276c7bf71f7a
Madi <110640717+braidfordm@users.noreply.github.com>
2022-11-21 10:02:28 +1300

    Add files via upload

--- 05_converting.py
+++ 05_converting.py
@@ -2,18 +2,43 @@ def conversion_checker():
     
     error = "Please chose valid measurement"
 
-# loop starts here
-while True:
-    response = input ("What are you convering from? ").lower()
+    # loop starts here
+    while True:
+        
+        response = input ("What unit are you convering from? ").lower()
 
-    if response == "mg" or  response == "g" or  response == "kg" or response == "t":
-        return ("weight")
+        if response == "mg" or  response == "g" or  response == "kg" or response == "t":
+            return ("weight")
 
-    elif response == "sec" or response =="min" or response == "hour":
-        return ("time")
+        elif response == "sec" or response =="min" or response == "hour":
+             return ("time")
 
-    elif 
+        elif response == "mm" or response == "cm" or response == "m" or response == "km":
+            return ("distance")
+        else:
+            print (error)
 
+def unit_checker():
+    
+    error = "Please chose valid measurement"
 
+    # loop starts here
+    while True:
+        
+        response = input ("What unit are you convering to? ").lower()
 
+        if response == "mg" or  response == "g" or  response == "kg" or response == "t":
+            return ("weight")
 
+        elif response == "sec" or response =="min" or response == "hour":
+             return ("time")
+
+        elif response == "mm" or response == "cm" or response == "m" or response == "km":
+            return ("distance")
+        else:
+            print (error)
+
+# main routine
+
+conversion = conversion_checker()
+print("you converting from", conversion "to" unit_checker)
